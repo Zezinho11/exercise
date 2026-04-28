a449f186f7046bac793a7b806b73b536fedd4e1b
José Suzuki <jcbrandine@gmail.com>
2026-04-28 15:01:54 -0300

    Buttons and forms creation

--- JS_exercise.js
+++ JS_exercise.js
@@ -1,1 +1,10 @@
 /* Lista de exemplos em java script */
+
+
+const btn = document.querySelector('button');
+btn.addEventListener('click', () => {
+    btn.textContent = 'Mim clicou ai ai ai ai';
+    setTimeout(() => {
+        btn.textContent = 'Mim aperta';
+    }, 1000)
+});
